932197a1f5cc9a32723ee90d45fc2276a6c339d5
Vladimir-cloudj <wizart88@gmail.com>
2023-04-23 21:15:07 +0400

    drop_down#2 completed

--- search_bar/script.js
+++ search_bar/script.js
@@ -1,11 +1,32 @@
-const icon = document.querySelector('.icon');
-const search = document.querySelector('.search');
-const clear = document.querySelector('.clear');
-
-icon.onclick = function() {
-    search.classList.toggle('active')
-};
-
-clear.onclick = function() {
-    document.getElementById('mySearch').value = ''
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtns = document.querySelectorAll('.menu__btn');
+    const drops = document.querySelectorAll('.dropdown');
+    menuBtns.forEach(el => {
+        el.addEventListener('click', (e) => {
+            let currentBtn = e.currentTarget;
+            let drop = currentBtn.closest('.menu__item').querySelector('.dropdown')
+            menuBtns.forEach(el => {
+                if (el !== currentBtn) {
+                    el.classList.remove('menu__btn--active')
+                }
+            })
+            drops.forEach(el => {
+                if (el !== drop) {
+                    el.classList.remove('dropdown--active')
+                }
+            })
+            drop.classList.toggle('dropdown--active')
+            currentBtn.classList.toggle('menu__btn--active')
+        })
+    })
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.menu')) {
+            menuBtns.forEach(el => {
+                    el.classList.remove('menu__btn--active')
+            })
+            drops.forEach(el => {
+                    el.classList.remove('dropdown--active')
+            })
+        }
+    })
+})
